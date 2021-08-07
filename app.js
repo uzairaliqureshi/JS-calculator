057d5d1e1f845186e055d92011c715a2da959c94
uzair27694 <uattari27694@gmail.com>
2021-08-07 10:47:56 +0500

    init

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 let screen = document.querySelector(".screen")
 
-document.querySelector(".equalOperator").addEventListener('click',()=>  
-screen.value = Function('return (' + screen.value + ')')())
+document.querySelector(".equalOperator").addEventListener('click', () =>
+  screen.value = Function('return (' + screen.value + ')')())
 
-document.getElementById('clearAll').addEventListener('click',()=>  screen.value="")
+document.getElementById('clearAll').addEventListener('click', () => screen.value = "")
 
-document.getElementById('clear1').addEventListener('click',()=> 
-screen.value =  screen.value.slice(0, -1))
+document.getElementById('clear1').addEventListener('click', () =>
+  screen.value = screen.value.slice(0, -1))
 
 let numberOfButtons = document.querySelectorAll(".btn").length;
 for (let i = 0; i < numberOfButtons; i++) {
-  document.querySelectorAll(".btn")[i].addEventListener("click", function() {
+  document.querySelectorAll(".btn")[i].addEventListener("click", function () {
     screen.value += this.value;
   });
 }
+
+document.addEventListener('keydown', function (event) {
+  let arrText = ["(", ")", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "+"]
+  if (arrText.includes(event.key)) {
+    screen.value += event.key;
+  }
+  else if (event.key === "Backspace") {
+    screen.value = screen.value.slice(0, -1)
+  }
+  else if (event.key === "Enter") {
+    screen.value = Function('return (' + screen.value + ')')()
+  }
+})
