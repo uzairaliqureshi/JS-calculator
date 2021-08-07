7347fb1534e6ddc127bc87bc1432b6021d876419
uzair27694 <uattari27694@gmail.com>
2021-08-07 11:53:32 +0500

    init

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-let screen = document.querySelector(".screen")
+let screen = document.querySelector(".screen");
 
 document.querySelector(".equalOperator").addEventListener('click', () =>
-  screen.value = Function('return (' + screen.value + ')')())
+  screen.value = Function('return (' + screen.value + ')')().toFixed(2).replace(".00", ""));
 
-document.getElementById('clearAll').addEventListener('click', () => screen.value = "")
+document.getElementById('clearAll').addEventListener('click', () => screen.value = "");
 
 document.getElementById('clear1').addEventListener('click', () =>
-  screen.value = screen.value.slice(0, -1))
+  screen.value = screen.value.slice(0, -1));
 
 let numberOfButtons = document.querySelectorAll(".btn").length;
 for (let i = 0; i < numberOfButtons; i++) {
@@ -15,15 +15,11 @@ for (let i = 0; i < numberOfButtons; i++) {
   });
 }
 
+//! for keyboard
+
 document.addEventListener('keydown', function (event) {
   let arrText = ["(", ")", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "+"]
-  if (arrText.includes(event.key)) {
-    screen.value += event.key;
-  }
-  else if (event.key === "Backspace") {
-    screen.value = screen.value.slice(0, -1)
-  }
-  else if (event.key === "Enter") {
-    screen.value = Function('return (' + screen.value + ')')()
-  }
+  if (arrText.includes(event.key)) screen.value += event.key;
+  if (event.key === "Backspace") screen.value = screen.value.slice(0, -1);
+  if (event.key === "Enter") screen.value = Function('return (' + screen.value + ')')().toFixed(2).replace(".00", "");
 })
