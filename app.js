9ad7f82ef522c37d5305816c0378503b252e3488
uzairaliqureshi <uattari27694@gmail.com>
2021-08-28 00:24:09 +0500

    init

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const opr = ["+", "/", "*", "-"];
 const numberOfBtn = document.querySelectorAll(".operator").length;
 for (let i = 0; i < numberOfBtn; i++) {
   document.querySelectorAll(".operator")[i].addEventListener("click", function () {
-    if (this.value !== screen.value.slice(-1)) {
+    if (!screen.value.endsWith(this.value)) {
       if (!opr.includes(screen.value.slice(-1))) {
         screen.value += this.value;
       } else {
-        changeOpr(this.value)
+        changeOpr(this.value);
       }
     }
   });
@@ -25,24 +25,24 @@ function dotMinus(argument) {
   if (argument === '-') screen.value += argument;
 };
 function changeOpr(argument) {
-  if (argument === "(" || argument === ")") { screen.value += argument }
+  if (argument === "(" || argument === ")") { screen.value += argument; }
   else if (opr.includes(screen.value.slice(-1)) && argument !== "." && argument !== "-" && !opr.includes(screen.value.slice(-2, -1))) {
     screen.value = screen.value.slice(0, -1);
     screen.value += argument;
   }
-  else { dotMinus(argument) }
+  else { dotMinus(argument); }
 };
 
 function calculation() {
   try {
     if (!opr.includes(screen.value.slice(-1))) {
-      screen.value = Function('return (' + screen.value + ')')().toFixed(2).replace(".00", "")
+      screen.value = Function('return (' + screen.value + ')')().toFixed(2).replace(".00", "");
     } else {
       const sliceBLC = screen.value.slice(0, -1);
-      screen.value = Function('return (' + sliceBLC + ')')().toFixed(2).replace(".00", "")
+      screen.value = Function('return (' + sliceBLC + ')')().toFixed(2).replace(".00", "");
     }
   } catch (error) {
-    screen.value = ""
+    screen.value = "";
   }
 };
 
@@ -52,10 +52,10 @@ document.querySelector(".equalOperator").addEventListener('click', () => calcula
 
 // //! for keyboard
 document.addEventListener('keydown', function (event) {
-  const opr = ["+", "/", "*", "(", ")"];
+  const opr = ["+", "/", "*", "(", ")","-"];
   const opr1 = ["+", "/", "*", "(", ")", ".", "-"];
   if (event.key !== screen.value.slice(-1)) {
-    if (opr.includes(event.key) && !opr.includes(screen.value.slice(-1))) screen.value += event.key;
+    if (opr.includes(event.key) && !opr.includes(screen.value.slice(-1))) screen.value += event.key; 
     else if (opr1.includes(event.key)) changeOpr(event.key);
   };
   const numText = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
